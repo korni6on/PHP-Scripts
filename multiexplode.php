2cd6fdc1fbc6005e53988e7ccbf80e0109f7a6e9
Ivelin Kostov <korni6on@users.noreply.github.com>
2016-01-10 13:12:12 +0200

    Create multiexplode.php

---
+++ multiexplode.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+  * @source http://php.net/manual/bg/function.explode.php
+function multiexplode ($delimiters,$string) {
+    
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
+
+?>
